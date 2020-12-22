3e50117f42b06eccb5999c8daae43eca6f07413e
treyshedrick <treyshedrick@gmail.com>
2020-12-21 23:00:12 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React To Do List
+First ReactJS page that shows tasks to do for the day.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Testing 1,2,3
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
