9a42e64e3b56b3aa5ef5b7e8169fa62400f0108f
treyshedrick <treyshedrick@gmail.com>
2020-12-23 20:32:23 -0600

    useState hook to add items to array

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,24 @@
 import List from "./List";
+import React, { useState } from 'react';
 
-const Todo= () => { 
-    const todoStat = ["Do This", "Do That", "Do everything"];
+const Todo= () => {
+    const [count, currentCount] = useState(0);
+    const [item, itemCounter] = useState([]);
+    
+    let itemCount = () =>{
+        itemCounter(listitems => [...listitems, "Test"])
+        currentCount(count + 1);
+    }
     return(
         <div>
-            {todoStat.map(x =>( <List item={x}/>
+            {item.map((x => <List item={x} key={Math.random()}/>
             ))}
+            <textarea id="userInput"></textarea>
+            <p>
+            <button onClick={itemCount}>Add Item</button>
+            </p>
+            <span>{count} items on list</span>
+
         </div>
     );
 };
