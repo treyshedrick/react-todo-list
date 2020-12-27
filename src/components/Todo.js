2171803211a844587a1d360c97d29fe0b5584d42
treyshedrick <treyshedrick@gmail.com>
2020-12-27 13:32:46 -0600

    form and new hook | todo list acting accordingly

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,21 +3,29 @@ import React, { useState } from 'react';
 
 const Todo= () => {
     const [count, currentCount] = useState(0);
+    const [individualItem, newItem] = useState("");
     const [item, itemCounter] = useState([]);
     
-    let itemCount = () =>{
+    const indivItem = i => {
+        newItem(i.target.value);
+    }
+
+    let itemCount = (event) =>{
         let listKey = `listKey${count.toString()}`
-        itemCounter(listitems => [...listitems, {itemTodo: "This", id: listKey}])
+        itemCounter(listitems => [...listitems, {itemTodo: individualItem, id: listKey}])
         currentCount(count + 1);
+        event.preventDefault();
     }
     return(
         <div>
             {item.map((x => <List item={x.itemTodo} key={x.id}/>
             ))}
-            <textarea id="userInput"></textarea>
+            <form onSubmit={itemCount}>
+            <textarea id="userInput" onChange={indivItem}></textarea>
             <p>
-            <button onClick={itemCount}>Add Item</button>
+            <button type="Submit">Add Item</button>
             </p>
+            </form>
             <span>{count} items on list</span>
 
         </div>
